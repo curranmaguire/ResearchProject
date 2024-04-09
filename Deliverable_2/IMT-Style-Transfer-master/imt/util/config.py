f0bafc54ba7185c858e1d052c12cbf8e4f22ef6f
curranmaguire <curran.maguire2001@gmail.com>
2024-04-09 10:21:19 +0100

    deleting the old shit models

--- Deliverable_2/IMT-Style-Transfer-master/imt/util/config.py
+++
@@ -1,6 +0,0 @@
-import yaml
-
-
-def load_yaml_config(path):
-    with open(path, 'r') as stream:
-        return yaml.safe_load(stream)
--- Deliverable_2/IMT-Style-Transfer-master/setup.py
+++
@@ -1,7 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name="imt",
-    version="0.0.1",
-    packages=setuptools.find_packages()
-)
